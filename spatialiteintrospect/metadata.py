f425ad37bb3f8b91fe415dca39ea5e97b1977c2d
dispiste <cesar.izq@gmail.com>
2016-11-07 20:00:48 +0100

    Update version to 1.0

--- spatialiteintrospect/metadata.py
+++ spatialiteintrospect/metadata.py
@@ -8,9 +8,9 @@ Information describing the project.
 package = 'spatialiteintrospect'
 project = "spatialite-introspect"
 project_no_spaces = project.replace(' ', '')
-version = '0.2'
-description = """Python library for easily retrieving table names, geometry
-columns, geometry tables, etc from a Spatialite database"""
+version = '1.0'
+description = """Python library for easily retrieving table names, \
+geometry columns, geometry tables, etc from a Spatialite database"""
 authors = ['Cesar Martinez Izquierdo']
 authors_string = ', '.join(authors)
 emails = []
